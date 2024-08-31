888d195ad23c1efa3d41405ea57a1c8d82669c0c
Sujit Pattanaik <sujit.pattanaik@argusoft.in>
2024-08-31 19:22:02 +0530

    jwt authentication feat added

--- Q6/customLogger.js
+++ Q6/customLogger.js
@@ -1,60 +1,57 @@
-const express = require("express");
 const winston = require("winston");
+const { combine, timestamp, printf, json, prettyPrint, errors, cli } =
+  winston.format;
+const DailyRotateFile = require("winston-daily-rotate-file");
 const morgan = require("morgan");
-require("winston-daily-rotate-file");
-
+const express = require("express");
 const app = express();
-
-const dailyRotateFileTransport = new winston.transports.DailyRotateFile({
-  filename: "logs/winston-%DATE%.log",
-  maxFiles: "1d",
-});
-
 const logger = winston.createLogger({
-  format: winston.format.combine(
-    winston.format.timestamp({
+  level: "info",
+  //   format: winston.format.cli(),
+  //   format: winston.format.json(),
+  format: combine(
+    timestamp({
       format: "YYYY-MM-DD hh:mm:ss",
     }),
-    winston.format.printf(
-      ({ timestamp, level, message }) => `${timestamp} -> ${level} : ${message}`
-    )
+    printf(
+      (el) => `[${el.timestamp}] ${el.level.toUpperCase()}: ${el.message}`
+    ),
+    errors({ stack: true })
   ),
   transports: [
-    new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.json()
-      ),
+    new winston.transports.Console({ format: cli() }),
+    // new winston.transports.File({ filename: "combined.log" }),
+    new DailyRotateFile({
+      filename: "logs/winston-%DATE%.log",
+      datePattern: "YYYY-MM-DD",
+      zippedArchive: true,
+      maxSize: "20m",
+      maxFiles: "15d",
     }),
-    // new winston.transports.File({ filename: "app.log" }),
-    dailyRotateFileTransport,
   ],
 });
-
-const morganFormat = ":method :url :status :response-time ms";
-
+const customFormat =
+  ":method :url :status :res[content-length] - :response-time ms";
 app.use(
-  morgan(morganFormat, {
+  morgan(customFormat, {
     stream: {
       write: (message) => {
         const logObject = {
           method: message.split(" ")[0],
           url: message.split(" ")[1],
           status: message.split(" ")[2],
-          responseTime: message.split(" ")[3],
+          responseSize: message.split(" ")[3],
+          responseTime: message.split(" ")[4],
         };
         logger.info(JSON.stringify(logObject));
       },
     },
   })
 );
-
 app.get("/logger", (req, res) => {
   res.send("He;lo");
 });
-
 const port = 3000;
-
 // CREATE SERVER
 app.listen(port, () => {
   console.log("Server is started...");
--- Q7/jwt.js
+++ Q7/jwt.js
@@ -1,49 +1,15 @@
 const jwt = require("jsonwebtoken");
 const Users = require("./userModel");
 const express = require("express");
-
 const app = express();
-
 app.use(express.json());
-
-app.post("/signup", async (req, res) => {
-  try {
-    const newUser = await Users.create(req.body);
-    sendResponse(newUser, 201, req, res);
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-app.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    if (!email || !password) {
-      throw new Error("Please provide required credentials!");
-    }
-
-    const user = await Users.findOne({ email }).select("+password");
-
-    if (!user || user.password !== password) {
-      throw new Error("Invalid credentials!");
-    }
-
-    sendResponse(user, 200, req, res);
-  } catch (error) {
-    console.log(error);
-  }
-});
-
 const signToken = (id) => {
   return jwt.sign({ id }, "what04-can13-they49-do299", {
     expiresIn: 1000,
   });
 };
-
 const sendResponse = (user, statusCode, req, res) => {
   const token = signToken(user._id);
-
   res.status(statusCode).json({
     status: "success",
     token,
@@ -52,9 +18,59 @@ const sendResponse = (user, statusCode, req, res) => {
     },
   });
 };
-
+app.post("/signup", async (req, res) => {
+  try {
+    const newUser = await Users.create(req.body);
+    sendResponse(newUser, 201, req, res);
+  } catch (error) {
+    console.log(error);
+  }
+});
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      throw new Error("Please provide required credentials!");
+    }
+    const user = await Users.findOne({ email }).select("+password");
+    if (!user || user.password !== password) {
+      throw new Error("Invalid credentials!");
+    }
+    sendResponse(user, 200, req, res);
+  } catch (error) {
+    console.log(error);
+  }
+});
+app.post("/authenticate", async (req, res, next) => {
+  // Read token
+  let token = req.headers.authorization;
+  if (token && token.startsWith("Bearer")) {
+    token = token.split(" ")[1];
+  }
+  if (!token) {
+    throw new Error("You have to login to access this route!");
+  }
+  // Validate token (Here decoded token stores the payload of the JWT)
+  const decodedToken = await util.promisify(jwt.verify)(
+    token,
+    "what04-can13-they49-do299"
+  );
+  // Check whether user exists or not
+  const user = await Users.findById(decodedToken.id);
+  if (!user) {
+    throw new Error(" User with the given token doesn't exist! ");
+  }
+  // Check whether user changed password after the token was issued
+  if (await user.isPasswordChanged(decodedToken.iat)) {
+    throw new Error(
+      " Password has been changed recently. Please login again! "
+    );
+  }
+  // Allow user to access route
+  req.user = user;
+  next();
+});
 const port = 3000;
-
 app.listen(port, () => {
   console.log("Server is started...");
 });
