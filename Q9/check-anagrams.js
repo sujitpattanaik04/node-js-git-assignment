2dbf5d77c98c81f72c11b1892abb43d513f859cf
Sujit Pattanaik <sujit.pattanaik@argusoft.in>
2024-09-04 09:59:58 +0530

    last check

--- Q9/check-anagrams.js
+++ Q9/check-anagrams.js
@@ -1,19 +1,20 @@
 function isAnagrams(str1, str2) {
-  let alphabets = "abcdefghijklmnopqrstuvwxyz";
+  let asciiOfa = "a".charCodeAt(0);
   let arr = new Array(26).fill(0);
+
   if (str1.length !== str2.length) {
     return false;
   }
-  for (let el of str1) {
-    arr[alphabets.indexOf(el)]++;
+  for (let el = 0; el < str2.length; el++) {
+    arr[str2.charCodeAt(el) - asciiOfa]++;
   }
-  for (let el of str2) {
-    arr[alphabets.indexOf(el)]--;
+  for (let el = 0; el < str1.length; el++) {
+    arr[str1.charCodeAt(el) - asciiOfa]--;
   }
   for (let el of arr) {
-    if (!el) return false;
+    if (el) return false;
   }
   return true;
 }
 
-console.log(isAnagrams("success", "succeec"));
+console.log(isAnagrams("eraa", "aree"));
